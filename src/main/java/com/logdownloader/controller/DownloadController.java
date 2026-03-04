29f6346cf26abd3c9df481066097f3e639e95a4f
mohitjani14 <imohitt14@gmail.com>
2026-03-04 15:44:54 +0530

    fixed error

--- src/main/java/com/logdownloader/controller/DownloadController.java
+++ src/main/java/com/logdownloader/controller/DownloadController.java
@@ -1,23 +1,26 @@
 package com.logdownloader.controller;
 
 import com.logdownloader.model.DownloadJob;
+import com.logdownloader.repository.DownloadJobRepository;
 import com.logdownloader.service.DownloadService;
+
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+
 import java.io.File;
 
 @RestController
-
-@Autowired
-private DownloadJobRepository downloadJobRepository;
-
 @RequestMapping("/api/download")
 public class DownloadController {
 
     private final DownloadService downloadService;
+
+    @Autowired
+    private DownloadJobRepository downloadJobRepository;
 
     public DownloadController(DownloadService downloadService) {
         this.downloadService = downloadService;
@@ -27,7 +30,7 @@ public class DownloadController {
     public DownloadJob startDownload(@PathVariable Long moduleId,
                                      @RequestParam String date) {
 
-    return downloadService.createJob(moduleId, date);
+        return downloadService.createJob(moduleId, date);
     }
 
     @GetMapping("/status/{jobId}")
@@ -39,18 +42,18 @@ public class DownloadController {
     @GetMapping("/file/{jobId}")
     public ResponseEntity<Resource> downloadFile(@PathVariable Long jobId) throws Exception {
 
-    DownloadJob job = downloadJobRepository.findById(jobId)
-            .orElseThrow(() -> new RuntimeException("Job not found"));
+        DownloadJob job = downloadJobRepository.findById(jobId)
+                .orElseThrow(() -> new RuntimeException("Job not found"));
 
-    String filePath = job.getFilePath();
+        String filePath = job.getFilePath();
 
-    File file = new File(filePath);
+        File file = new File(filePath);
 
-    Resource resource = new UrlResource(file.toURI());
+        Resource resource = new UrlResource(file.toURI());
 
-    return ResponseEntity.ok()
-            .header(HttpHeaders.CONTENT_DISPOSITION,
-                    "attachment; filename=" + file.getName())
-            .body(resource);
-}
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=" + file.getName())
+                .body(resource);
+    }
 }
