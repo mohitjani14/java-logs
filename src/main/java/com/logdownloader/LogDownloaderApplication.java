09886f931fef1a4d6dfd4f3e63f7b5dafb45cc6b
mohitjani14 <imohitt14@gmail.com>
2026-03-04 12:28:48 +0530

    initial commit

---
+++ src/main/java/com/logdownloader/LogDownloaderApplication.java
@@ -0,0 +1,13 @@
+package com.logdownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogDownloaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogDownloaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/logdownloader/LogDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logdownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogDownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
